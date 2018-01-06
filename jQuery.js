0c696a3da660007eccee2472caa11d2354ad22bc
Tyler Fuqua <tyler@agilityda.com>
2018-01-05 23:59:16 -0700

    Stretch Goal #2

--- jQuery.js
+++ jQuery.js
@@ -14,8 +14,8 @@ $(document).ready(function() {
 
 //Stretch Goal #1
 
-$(document).ready(function() {
-    $(square).mouseenter(function() {
+ $(document).ready(function() {
+    $(square).hide(function() {
         $(this).css("background", "green");
     });
     $(square).mouseleave(function() {
@@ -24,10 +24,54 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-    $(squares).mouseenter(function() {
+    $(squares).hide(function() {
         $(this).css("background", "green");
     });
     $(squares).mouseleave(function() {
         $(this).css("background", "blue");
+    });
+}); 
+
+$(document).ready(function() {
+    $(".testPage").hide(function() {
+        
+    });
+});
+
+// Stretch Goal #2
+
+$(document).ready(function() {
+    $(element1).click(function() {
+        $(this).css("color", "red");
+    });
+});
+
+$(document).ready(function() {
+    $(element2).dblclick(function() {
+        $(this).css("font-size", "30px");
+    });
+});
+
+$(document).ready(function() {
+    $(element3).hover(function() {
+        $(this).css("background", "blue");
+    });
+});
+
+$(document).ready(function() {
+    $(element4).mouseenter(function() {
+        $(this).css("color", "yellow");
+    });
+});
+
+$(document).ready(function() {
+    $(element5).mouseleave(function() {
+        $(this).css("color", "orange");
+    });
+});
+
+$(document).ready(function() {
+    $(element6).click(function() {
+        $(this).css("font-family", "Futura");
     });
 });
